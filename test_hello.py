504bed346cfc7cf5a0e60d5a4ef2e828968735be
Thierry Celestin LEGUE DOHO <leguethierry93@gmail.com>
2025-06-20 15:27:39 +0200

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -13,10 +13,10 @@ def teardown_function(function):
 
 ### Run to see failed test
 #def test_hello_add():
-#    assert add(test_hello_add1.x) == 12
+#    assert add1(test_hello_add1.x) == 12
 
 def test_hello_subtract():
     assert subtract(test_hello_subtract.x) == 9
 
 def test_add2():
-    assert add(1, 2) == 3
+    assert add2(1, 2) == 3
