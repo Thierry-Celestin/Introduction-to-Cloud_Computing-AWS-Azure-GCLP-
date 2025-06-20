bd46ae73328784b5d4dcf8cd2126542d39571c42
Thierry Celestin LEGUE DOHO <leguethierry93@gmail.com>
2025-06-20 15:18:24 +0200

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,22 @@
-from hello import add
+from hello import add2, add1, toyou, subtract 
 
 
-def test_add():
+def setup_function(function):
+    print("Running Setup: %s" % {function.__name__})
+    function.x = 10
+
+
+def teardown_function(function):
+    print("Running Teardown: %s" % {function.__name__})
+    del function.x
+
+
+### Run to see failed test
+#def test_hello_add():
+#    assert add(test_hello_add1.x) == 12
+
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9
+
+def test_add2():
     assert add(1, 2) == 3
